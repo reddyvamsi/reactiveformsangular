89b6c394f91e5990180c6d6e24e9514174e80bc8
vamsikrishna <reddyvamsi480@gmail.com>
2024-01-03 16:34:41 +0530

    form error validations work done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(){}
   title = 'angularreactiveforms';
+  reactiveforms:FormGroup;
+ngOnInit(){
+  this.reactiveforms=new FormGroup({
+     firstname:new FormControl('', Validators.required),
+     lastname:new FormControl('', Validators.required),
+     email:new FormControl('',[Validators.required,Validators.email]),
+     username:new FormControl(''),
+     dateofbirth:new FormControl(''),
+     gender:new FormControl(''),
+     address:new FormControl(''),
+     country:new FormControl(''),
+     city:new FormControl('')
+  })
+
+}
+formsubmission(){
+  console.log(this.reactiveforms);
+}
 }
